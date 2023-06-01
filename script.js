76fd98313467f6d81d150b1eb1f371d400533022
Brian Salomone <brian.j.salomone@gmail.com>
2023-06-01 11:34:35 -0700

    added grid populate function but needs fix

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
-const gridContainer = document.querySelector(".grid-container")
-
+const gridContainer = document.querySelector("#container")
 gridContainer.style.height = "800px"
 gridContainer.style.width = "800px"
 gridContainer.style.border = "solid black"
 
-console.log(isPerfectSquare(64))
+const gridButton = document.querySelector("#grid-button")
 
-function populateGrid(numberOfSquares) {
-    numberOfSquares = 64
+let numberOfSquares = 64
+
+gridButton.addEventListener('click', ()=> {
+    populateGrid(64)
+})
+
+function populateGrid(squareCount) {
+    numberOfSquares = squareCount
 
     if (!isPerfectSquare) {
-        alert("Must enter a perfect square (e.g. 64, 81, 100")
+        alert("Must enter a perfect square (e.g. 64, 81, 100)")
     } else {
-        
+        for (let i=0; i<Math.sqrt(numberOfSquares); i++) {
+            gridContainer.classlist.add('square')
+            console.log(gridContainer.className)
+        }
     }
 }
 
 function isPerfectSquare(number) {
     return (Number.isInteger(Math.sqrt(number)))
 }
+
+//Relies on function to be created
+const square = gridContainer.querySelector(".square")
+let squareSize = Math.sqrt(numberOfSquares)
+square.style.width = "squareSizepx"
+square.style.height = "squareSizepx"
+square.style.border = "solid black"
+
+gridContainer.appendChild(square)
